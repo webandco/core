8d0d1d547a8c0897529bee22774fcf329d7658cf
Robin Appelman <icewind@owncloud.com>
2015-02-09 15:15:03 +0100

    properly return false if we cant delete the source file

--- lib/private/files/storage/common.php
+++ lib/private/files/storage/common.php
@@ -492,9 +492,9 @@ abstract class Common implements \OC\Files\Storage\Storage {
 		$result = $this->copyFromStorage($sourceStorage, $sourceInternalPath, $targetInternalPath, true);
 		if ($result) {
 			if ($sourceStorage->is_dir($sourceInternalPath)) {
-				$sourceStorage->rmdir($sourceInternalPath);
+				$result &= $sourceStorage->rmdir($sourceInternalPath);
 			} else {
-				$sourceStorage->unlink($sourceInternalPath);
+				$result &= $sourceStorage->unlink($sourceInternalPath);
 			}
 		}
 		return $result;
